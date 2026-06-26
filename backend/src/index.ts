e7c750729fc448178447c67ad5949bd28ed64405
Anish Dey <deyanis669@gmail.com>
2026-06-26 23:35:08 +0530

    intialize handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+	return c.text('signup route')
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.post('/api/v1/blog', (c) => {
+	return c.text('create blog route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('update blog route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	return c.text('get blog route')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('get bulk blog route')
 })
 
 export default app
